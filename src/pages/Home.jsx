4d1c6da3538e71f02c73d4b56c430a5a13a17aaa
skeeven1 <skeeven.men@efrei.net>
2024-03-25 12:32:10 +0100

    remplacement par les bons fichiers

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import homeStore from '../stores/homeStore'
+
+export default function Home() {
+    const store = homeStore()
+   
+    React.useEffect(() => {
+        store.fetchCoins()
+    }, [])
+
+  return (
+    <div>
+        {store.coins.map(coin => {
+            return (
+            <div key={coin.id}>
+            <Link to={`/${coin.id}`}>
+            {coin.name}
+            </Link>
+            </div>
+        )  
+        })}
+        
+    </div>
+  )
+}
